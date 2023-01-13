9b8c483448cbfe035704caf4908c907eab90bace
jmlibued <jmlibued257@gmail.com>
2023-01-13 09:02:36 +0800

    connection

--- pdo-connection.php
+++ pdo-connection.php
@@ -1,6 +1,6 @@
 <?php
 // localhost
-$connect = new PDO('mysql:host=localhost;dbname=tabulation', 'root', '');
+// $connect = new PDO('mysql:host=localhost;dbname=tabulation', 'root', '');
 // webhost
-// $connect = new PDO('mysql:host=localhost;dbname=u231790904_tabulation', 'u231790904_tabulation', 'Admin123');
+$connect = new PDO('mysql:host=localhost;dbname=u231790904_tabulation', 'u231790904_tabulation', 'Admin123');
 session_start();
